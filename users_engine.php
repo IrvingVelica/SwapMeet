e339db37e59ec811b707fc429c7a365da4f79056
kdavidmorales13 <dmorales1@uabc.edu.mx>
2020-05-11 21:22:08 -0700

    Se agregaron funciones para la gestion de usuarios como: verificar si el usuario tiene una cuenta para inicio de session, de ser asi se obtiene el id de la cuenta, esto para empezar a trabajar con _SESSION

--- users_engine.php
+++ users_engine.php
@@ -1,21 +1,39 @@
 <?php
 include("conexiondb.php");
-
-$user_name = $_POST['user_name'];
-$user_lastname = $_POST['user_lastname'];
-$user_password =  $_POST['user_password'];
-$user_age =  $_POST['user_age'];
-$user_email =  $_POST['user_email'];
-$user_address =  $_POST['user_address'];
+$actions = $_POST['actions'];
 
 
-$insertUser = 'INSERT INTO users_data (user_id, first_name, last_name, password, age, email, address) 
+if($actions == 'insert') {
+	$user_name = $_POST['user_name'];
+	$user_lastname = $_POST['user_lastname'];
+	$user_password =  $_POST['user_password'];
+	$user_age =  $_POST['user_age'];
+	$user_email =  $_POST['user_email'];
+	$user_address =  $_POST['user_address'];
+
+	$sql_request = 'INSERT INTO users_data (user_id, first_name, last_name, password, age, email, address) 
 	VALUES(NULL, "'.$user_name.'", "'.$user_lastname.'", "'.$user_password.'",'.$user_age.',"'.$user_email.'","'.$user_address.'" )';
+	mysqli_query($conexion, $sql_request);
 
-if (mysqli_query($conexion, $insertUser)) {	
-	echo json_encode(1);
-} else {
-	echo json_encode(0);
+	if(mysqli_query($conexion, $sql_request)){
+		echo json_encode(1);
+	}else{
+		echo json_encode(2);
+	}
+}
+if($actions == 'login'){
+	$email_log = $_POST['email_log'];
+	$password_log = $_POST['password_log'];
+
+    $sql_request = 'SELECT * FROM users_data where email="'.$email_log.'" AND password="'.$password_log.'"';
+	$execute=mysqli_query($conexion, $sql_request);
+	$row=mysqli_fetch_assoc($execute);
+
+	if(mysqli_num_rows($execute)){
+		echo json_encode($row);
+	}else{
+		echo json_encode(2);
+	}
 }
 
 ?>
